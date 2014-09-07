102f3c4e193dee589235c51f01538655e2ef72e8
namihira <namihira@gmail.com>
2014-09-07 18:18:57 +0900

    added ex1-8 answer.

---
+++ java8-ex1-8/src/jp/co/namihira/java8/Main.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2014 Kohsuke Namihira All Rights Reserved.
+ */
+/**
+ * Q.
+ *  ラムダ式が次のような拡張forループ内の値をキャプチャした場合には、どうなりますか。
+ *      String[] names = { "Peter", "Paul", "Mary" };
+ *      List<Runnable> runners = new ArrayList<>();
+ *      for (String name : names)
+ *          runners.add(() -> System.out.println(name));
+ *  これは正当なコードでしょうか。各ラムダ式は異なる値をキャプチャするのでしょうか？
+ *  それとも、すべてのラムダ式が最後の値をキャプチャするのでしょうか。
+ *  従来のfor(int i = 0; i < names.length; i++)ループを使用すると、どうなるでしょうか？
+ */
+/**
+ * A.
+ *  正当なコード。各ラムダ式は異なる値をキャプチャできる。
+ *  従来のforループでも同じ動作。
+ */
+
+package jp.co.namihira.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // prepare
+        // - nothing
+
+        /*** 拡張for文 ***/
+        // action
+        String[] names = { "Peter", "Paul", "Mary" };
+        List<Runnable> runners = new ArrayList<Runnable>();
+        for (String name : names)
+            runners.add(() -> System.out.println(name));
+
+        // check
+        runners.stream().forEach(runner -> runner.run());
+
+
+        /*** for文 ***/
+        // action
+        String[] namesFor = { "Peter", "Paul", "Mary" };
+        List<Runnable> runnersFor = new ArrayList<Runnable>();
+        for (int i = 0; i < namesFor.length; i++) {
+            int index = i;
+            runnersFor.add(() -> System.out.println(namesFor[index]));
+        }
+
+        // check
+        runnersFor.stream().forEach(runner -> runner.run());
+    }
+
+
+}
