b2b621893b637829238c06daf4b9ce3d79ac07fb
namihiran <namihiran@gmail.com>
2014-10-24 00:53:40 +0900

    メソッド呼び出しを最適化しました。

--- java8-ex2-10/src/jp/co/namihira/java8/StreamUtils.java
+++ java8-ex2-10/src/jp/co/namihira/java8/StreamUtils.java
@@ -14,6 +14,7 @@ public class StreamUtils {
     /**
      * 与えられた値のリストに対して、平均値を返却します。
      * リストがnullまたはサイズが0の場合、0.0を返却します。
+     *
      * @param values
      * @return　平均値
      */
@@ -22,9 +23,7 @@ public class StreamUtils {
             return 0.0;
         }
 
-//      以下のような計算式で十分だが、問題の意図を考慮してstream().count()を使用した。
-//      final long SIZE = values.size();
-        final long SIZE = values.stream().count();
+        final long SIZE = values.size();
         Double avg = values.stream().reduce(0.0,
                 (total, value) -> total + (value / SIZE),
                 (total1, total2) -> total1 + total2);
