7b3834397bce1a18b6437fff4cd06cd4d94e998f
namihiran <namihiran@gmail.com>
2014-12-29 11:53:55 +0900

    再帰呼び出しから、for文に変更しました。

--- java8-ex3-09/src/jp/co/namihira/java8/ComparatorUtils.java
+++ java8-ex3-09/src/jp/co/namihira/java8/ComparatorUtils.java
@@ -5,10 +5,7 @@
 package jp.co.namihira.java8;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Comparatorに関するユーティリティクラス
@@ -18,19 +15,19 @@ public class ComparatorUtils {
     /**
      * 指定された順序で、指定されたフィールドを比較するコンパレータを生成します。
      *
-     * @param fieldname 比較対象のフィールド
+     * @param fieldnames 比較対象のフィールド
      * @return　コンパレータ
      *
      * @throws IllegalArgumentException fieldnameがnullの場合。
      *
      */
     @SuppressWarnings("unchecked")
-    public static <T> Comparator<T> lexicographicComparator(String... fieldname) {
-        if (fieldname == null) {
+    public static <T> Comparator<T> lexicographicComparator(String... fieldnames) {
+        if (fieldnames == null) {
             throw new IllegalArgumentException("fieldname must not be null");
         }
 
-        if (fieldname.length == 0) {
+        if (fieldnames.length == 0) {
             throw new IllegalArgumentException("fieldname must not be empty");
         }
 
@@ -41,24 +38,22 @@ public class ComparatorUtils {
 
             Comparable<T> name1;
             Comparable<T> name2;
-            try {
-                name1 = (Comparable<T>) getAccessibleField(obj1, fieldname[0]);
-                name2 = (Comparable<T>) getAccessibleField(obj2, fieldname[0]);
-            } catch (IllegalArgumentException | NoSuchFieldException  e) {
-                throw new IllegalArgumentException(e);
-            } catch (ReflectiveOperationException e) {
-                throw new RuntimeException(e);
-            }
+            for (String fieldname : fieldnames) {
+                try {
+                    name1 = (Comparable<T>) getAccessibleField(obj1, fieldname);
+                    name2 = (Comparable<T>) getAccessibleField(obj2, fieldname);
+                } catch (IllegalArgumentException | NoSuchFieldException  e) {
+                    throw new IllegalArgumentException(e);
+                } catch (ReflectiveOperationException e) {
+                    throw new RuntimeException(e);
+                }
 
-            final int result = name1.compareTo((T) name2);
-            if (result == 0) {
-                List<String> list = new LinkedList<>(Arrays.asList(fieldname));
-                list.remove(0);
-                return (list.size() == 0) ?
-                        result : lexicographicComparator(list.toArray(new String[0])).compare(obj1, obj2);
-            } else {
-                return result;
+                final int result = name1.compareTo((T) name2);
+                if (result != 0) {
+                    return result;
+                }
             }
+            return 0;
         };
 
         return (Comparator<T>) comp;
