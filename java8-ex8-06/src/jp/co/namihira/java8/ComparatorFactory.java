433688bcc6aab4058f30842fbae89d65a11befba
namihiran <namihiran@gmail.com>
2015-04-01 07:29:36 +0900

    javadoc追加しました。

--- java8-ex8-06/src/jp/co/namihira/java8/ComparatorFactory.java
+++ java8-ex8-06/src/jp/co/namihira/java8/ComparatorFactory.java
@@ -10,17 +10,24 @@ import java.util.Comparator;
 
 public class ComparatorFactory {
 
+    /**
+     * Point2Dに対する全順序（total ordering）なコンパレータを返します。
+     * @return Point2Dに対する全順序（total ordering）なコンパレータ
+     */
     public static Comparator<Point2D> getPoint2DComparator(){
         return Comparator.comparingDouble(Point2D::getX)
                          .thenComparingDouble(Point2D::getY);
     }
 
+    /**
+     * Rectangle2Dに対する全順序（total ordering）なコンパレータを返します。
+     * @return Rectangle2Dに対する全順序（total ordering）なコンパレータ
+     */
     public static Comparator<Rectangle2D> getRectangle2DComparator(){
         return Comparator.comparingDouble(Rectangle2D::getX)
                          .thenComparingDouble(Rectangle2D::getY)
                          .thenComparingDouble(Rectangle2D::getHeight)
                          .thenComparingDouble(Rectangle2D::getWidth);
     }
-
 
 }
